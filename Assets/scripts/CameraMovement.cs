8f21f7201e4559a3f2ca0e369fec4e5f72ee7c65
Polbar <polbarpolb@gmail.com>
2024-02-07 09:27:44 +0100

    camera finish

--- Assets/scripts/CameraMovement.cs
+++ Assets/scripts/CameraMovement.cs
@@ -7,6 +7,8 @@ public class CameraMovement : MonoBehaviour
     public Transform playerTramsform;
     public Vector3 offset;
              //vector3 es para tema de cordenadas asi que el vector 3 guarda para modificar el eje x y z
+    public Vector2 minCameraPosition;
+    public Vector2 maxCameraPosition;
     // Start is called before the first frame update
     void Start()
     {
@@ -17,6 +19,12 @@ public class CameraMovement : MonoBehaviour
     void Update()
     {
         Vector3 desiredPosition = playerTramsform.position + offset;
-        transform.position = desiredPosition;
+
+        float clampX = Mathf.Clamp(desiredPosition.x, minCameraPosition.x, maxCameraPosition.x);
+        float clampY = Mathf.Clamp(desiredPosition.y, minCameraPosition.y, maxCameraPosition.y);
+        //la linia anterior limita los ejes a los valores que le he dado en especifico el limitador es el Mathf.Clamp
+
+        Vector3 clampedPosition = new Vector3(clampX, clampY, desiredPosition.z);
+        transform.position = clampedPosition;
     }
 }
