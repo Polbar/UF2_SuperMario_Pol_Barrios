3d925a5741686adb35bba9a5db22a3827fad0b5c
Polbar <polbarpolb@gmail.com>
2024-02-19 12:15:38 +0100

    marios sounds

--- Assets/scripts/CameraMovement.cs
+++ Assets/scripts/CameraMovement.cs
@@ -4,11 +4,11 @@ using UnityEngine;
 
 public class CameraMovement : MonoBehaviour
 {
-    public Transform playerTransform;
+    public Transform playerTrasnform;
     public Vector3 offset;
-             //vector3 es para tema de cordenadas asi que el vector 3 guarda para modificar el eje x y z
     public Vector2 minCameraPosition;
     public Vector2 maxCameraPosition;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -18,16 +18,16 @@ public class CameraMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if(playerTransform != null)
+        if(playerTrasnform != null)
         {
-        Vector3 desiredPosition = playerTransform.position + offset;
+             Vector3 desiredPosition = playerTrasnform.position + offset;
 
         float clampX = Mathf.Clamp(desiredPosition.x, minCameraPosition.x, maxCameraPosition.x);
         float clampY = Mathf.Clamp(desiredPosition.y, minCameraPosition.y, maxCameraPosition.y);
-        //la linia anterior limita los ejes a los valores que le he dado en especifico el limitador es el Mathf.Clamp
-
-        Vector3 clampedPosition = new Vector3(clampX, clampY, desiredPosition.z);
+        // Sirve para poner limites para la camara
+        Vector3 clampedPosition = new Vector3 (clampX, clampY, desiredPosition.z);
         transform.position = clampedPosition;
+
         }
     }
 }
--- Assets/scripts/Enemy.cs
+++ Assets/scripts/Enemy.cs
@@ -3,17 +3,21 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Enemy : MonoBehaviour
-
 {
-    public Rigidbody2D rBody;
-
-    public float enemySpeed = 5;
-
-    public float enemyDirection = 1;
+    private Rigidbody2D rBody;
+    public float enemySpeed = 2;
+    private float enemyDirection = 1;
+    public Animator anim;
+    private AudioSource source; 
+    public AudioClip deathSound;
+    private BoxCollider2D boxCollider;
+    
     // Start is called before the first frame update
     void Awake()
     {
         rBody = GetComponent<Rigidbody2D>();
+        source = GetComponent<AudioSource>();
+        boxCollider = GetComponent<BoxCollider2D>();
     }
 
     // Update is called once per frame
@@ -21,25 +25,31 @@ public class Enemy : MonoBehaviour
     {
         rBody.velocity = new Vector2(enemyDirection * enemySpeed, rBody.velocity.y);
     }
-
     void OnCollisionEnter2D(Collision2D collision)
     {
-        if(collision.gameObject.layer == 3 || collision.gameObject.layer == 6 || collision.gameObject.layer == 7)
+        if(collision.gameObject.layer == 3 ||collision.gameObject.tag == "Goombas")
         {
-            if(enemyDirection == 1)
-            {
-                enemyDirection = -1;
-            }
-            else if(enemyDirection == -1)
-            {
-                enemyDirection = 1;
-            }
+         if(enemyDirection == 1)
+        {
+            enemyDirection = -1;
         }
-
+        else if(enemyDirection == -1)
+        {
+            enemyDirection = 1;
+        }
+        }
         if(collision.gameObject.tag == "Player")
         {
             Destroy(collision.gameObject);
         }
+    }
 
+    public void GoombaDeath()
+    {
+        source.PlayOneShot(deathSound);
+        boxCollider.enabled = false;
+        rBody.gravityScale = 0;
+        enemyDirection = 0;
+        Destroy(gameObject, 0.5f);
     }
 }
