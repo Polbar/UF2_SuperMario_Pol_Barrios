d581c7d5396c89ede50ef1eaf161151fc51bd697
Polbar <polbarpolb@gmail.com>
2024-02-26 14:01:34 +0100

    puntuacion prueba

--- Assets/Scripts/MenuManager.cs
+++ Assets/Scripts/MenuManager.cs
@@ -1,27 +1,63 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
 public class MenuManager : MonoBehaviour
 {
-    public Text scoureText;
-    public int score;
+    public Text scoreText;
+    private int score;
 
-    // Start is called before the first frame update
+    // Etiqueta del objeto que al ser destruido incrementará la puntuación
+    public string tagObjetivo;
+
     void Start()
     {
         LoadScore();
     }
 
+    void Update()
+    {
+        // Actualiza el texto de la puntuación en cada frame
+        UpdateScoreText();
+    }
+
     void LoadScore()
     {
-        scoureText.text = "Puntuación: " + score.ToString();
+        // Carga la puntuación desde algún lugar, si es necesario.
+        // Por ejemplo, desde un archivo guardado o una base de datos.
+        // Aquí simplemente inicializamos la puntuación a cero.
+        score = 0;
+    }
+
+    void UpdateScoreText()
+    {
+        // Actualiza el texto con la puntuación actual
+        scoreText.text = "Puntuación: " + score.ToString();
     }
 
     public void LoadFirstLevel()
     {
+        // Carga el primer nivel
         SceneManager.LoadScene("Nivel 1");
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        // Verifica si el jugador colisiona con un objeto que tenga la etiqueta "Coin" o "Goombas_1"
+        if (other.CompareTag("Coin") || other.CompareTag("Goombas_1"))
+        {
+            // Incrementa la puntuación
+            score++;
+        }
+    }
+
+    void OnDestroy()
+    {
+        // Verifica si el objeto destruido tiene la etiqueta correcta
+        if (gameObject.CompareTag(tagObjetivo))
+        {
+            // Incrementa la puntuación
+            score++;
+        }
     }
 }
